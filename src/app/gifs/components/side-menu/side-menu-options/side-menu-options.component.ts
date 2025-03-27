cc7fab270e4a0a69056f724982eddabd8e9f1fd9
DJReyes10 <djreyesp@unah.hn>
2025-03-27 14:33:39 -0600

    Cambios en el menu, se secciono el side en componentes.

--- src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -1,8 +1,37 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption {
+  label: string;
+  subLabel: string;
+  route: string;
+  icon: string;
+}
+
+
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [ RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.component.html',
 })
-export class GifsSideMenuOptionsComponent { }
+export class GifsSideMenuOptionsComponent {
+menuOptions:MenuOption[]= [
+{
+  label: 'Trending',
+  subLabel: 'Gifs Popular',
+  route: '/dashboard/trending',
+  icon: 'fa-solid fa-chart-line',
+  
+},
+{
+  label: 'Search',
+  subLabel: 'Search gifs',
+  route: '/dashboard/search',
+  icon: 'fa-solid fa-magnifying-glass',
+  
+},
+]
+
+
+ }
