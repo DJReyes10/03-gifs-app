ecd0c65a8ee17a47bfa4d89338d107367c60c407
DJReyes10 <djreyesp@unah.hn>
2025-03-28 15:22:06 -0600

    Se cargaron los gifs y tambien se creo un servicio el cual me ayuda a traer dichos gifs.

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,15 +1,22 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from 'src/environments/environment.development';
 import type { GiphyReponse } from '../interfaces/giphy.interface';
+import { Gif } from '../interfaces/gif.interface';
+import { GifMapper } from '../mapper/gif.mapper';
 
 @Injectable({providedIn: 'root'})
 export class GifService {
 
     private http = inject(HttpClient)
 
+
+    trendingGifs = signal<Gif[]>([])
+    trendingGifsLoading = signal(true);
+
     constructor(){
         this.loadTrendingGifs();
+        console.log('Servicio Contratado')
     }
 
  loadTrendingGifs(){ //Es un objeto que permite realizar las peticiones get, put, delet y patch
@@ -18,7 +25,15 @@ export class GifService {
             api_key: environment.giphyApiKey,
             limit: 20,
         }
-    })
+    }).subscribe( (resp) => { //callback
+        // resp.data[0].images.original.url
+
+        const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
+        this.trendingGifs.set(gifs);
+        this.trendingGifsLoading.set(false);
+
+        console.log({ gifs });
+    } )
  }
     
 }
